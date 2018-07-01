010b8944021ba35e1abe13c0ae151c2de727d6bb
Rob Pickerill <rob.pickerill@linuxadept.co.uk>
2018-07-01 08:27:43 +0100

    add terraform s3 state module

---
+++ terraform-state-s3/main.tf
@@ -0,0 +1,27 @@
+# Terraform module for managing s3 bucket and dynamodb table for the state store
+
+resource "aws_s3_bucket" "terraform" {
+  bucket = "${var.s3_bucket}"
+
+  tags {
+    Name        = "${var.s3_bucket_name}"
+    Environment = "${var.env}"
+    Terraform   = "Created by Terraform"
+  }
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_dynamodb_table" "terraform" {
+  name           = "${var.dynamodb_table}"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
