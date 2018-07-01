0fef70077a465482b20c3c859ae77dfff2d3870c
Rob Pickerill <rob.pickerill@linuxadept.co.uk>
2018-07-01 09:20:03 +0100

    update module for sse and pit

--- terraform-state-s3/main.tf
+++ terraform-state-s3/main.tf
@@ -1,10 +1,10 @@
 # Terraform module for managing s3 bucket and dynamodb table for the state store
 
-resource "aws_s3_bucket" "terraform" {
-  bucket = "${var.s3_bucket}"
+resource "awsS3Bucket" "terraform" {
+  bucket = "${var.s3Bucket}"
 
   tags {
-    Name        = "${var.s3_bucket_name}"
+    Name        = "${var.s3BucketName}"
     Environment = "${var.env}"
     Terraform   = "Created by Terraform"
   }
@@ -14,11 +14,13 @@ resource "aws_s3_bucket" "terraform" {
   }
 }
 
-resource "aws_dynamodb_table" "terraform" {
-  name           = "${var.dynamodb_table}"
-  read_capacity  = 5
-  write_capacity = 5
-  hash_key       = "LockID"
+resource "awsDynamodbTable" "terraform" {
+  name           	 = "${var.dynamodbTable}"
+  read_capacity  	 = 5
+  write_capacity 	 = 5
+  hash_key       	 = "LockID"
+  point_in_time_recover  = true
+  server_side_encryption = true
 
   attribute {
     name = "LockID"
